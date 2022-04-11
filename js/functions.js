21937d513ee6e47a0376461867e29a43c7babae7
Thiagomatheus12 <thi_ago5@hotmail.com>
2022-04-11 17:26:45 -0300

    criei mais exercicios

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+//criar um aplicativo de frases motivacionais
+
+//Declararion - Declaração da função
+//function statement
+function createPhrase() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+// executar a função
+//execute, run, call, invoke
+
+createPhrase()
+createPhrase()
+createPhrase()
---
+++ js/functions2.js
@@ -0,0 +1,17 @@
+//function expression
+//funtion anonymous
+
+//parâmetros (parameters)
+const sum = function(number1, number2) {
+    total =  number1 + number2;
+    return total;
+
+}
+
+
+let number1 = 10;
+let number2 = 20;
+console.log(`o número 1 é ${number1}`);
+console.log(`o número 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`);
+//sum(2, 3) //arguments
