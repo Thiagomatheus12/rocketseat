42067ddcad7249abb1178dc4ab117152b0bee01f
Thiagomatheus12 <thi_ago5@hotmail.com>
2022-04-11 15:58:49 -0300

    criei exercicios

---
+++ js/agrupando.js
@@ -0,0 +1,30 @@
+console.log(Infinity);
+
+// Variáveis e tipos de dados
+// Declaração or Declaration
+
+var name
+
+//assigment or atribuição de valores
+name = "Thiago"
+
+// que tipo de dado foi colocado na variável
+
+//console.log(typeof name)
+
+// agrupamento de declarções 
+
+let age, isHuman
+
+age = 20
+isHuman = true
+//multiplos argumentos na função
+//console.log(name, age, isHuman)
+
+//escrita de texto + variáveis
+
+//concatenando valores
+console.log('o '+ name + ' tem ' + age + ' anos.')
+
+//interpolando valores com template literals or template strings
+console.log(`o ${name} tem ${age} anos.`)
---
+++ js/object.js
@@ -0,0 +1,10 @@
+// Object
+
+const person = {
+    name: 'John',
+    age: 20,
+    weight: 88.6,
+    isAdmin: true,
+}
+
+console.log(`${person.name} tem ${person.age} anos`)
